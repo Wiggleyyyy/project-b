b9b5ca1b201ec0acf60dfd5b0028a4c507d37415
Wiggleyyyy <141311953+Wiggleyyyy@users.noreply.github.com>
2023-10-17 11:44:00 +0200

    initial commits

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import *
+import PIL
+from PIL import *
+
+# ==== WINDOW CONFIGS ====
+game_title="PROJECT B | (INSERT VERSION LATER)"
+root=tk.Tk()
+root.title(game_title)
+root.geometry("1920x1080")
+
+root.mainloop()
