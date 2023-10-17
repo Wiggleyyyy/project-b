78fa80c04256b012f6ee5389d5ebb7602778a2a8
Wiggleyyyy <141311953+Wiggleyyyy@users.noreply.github.com>
2023-10-17 12:18:29 +0200

    added pygame module to main

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-import tkinter as tk
-from tkinter import *
-import PIL
-from PIL import *
+import pygame
+pygame.init()
 
-# ==== WINDOW CONFIGS ====
-game_title="PROJECT B | (INSERT VERSION LATER)"
-root=tk.Tk()
-root.title(game_title)
-root.geometry("1920x1080")
+game_title="PROJECT B | (INSERT VERSION OF GAME LATER)" # ==== TITLE NEEDS TO BE CHANGED ====
+screen=pygame.display.set_mode([1920, 1080])
+pygame.display.set_caption(game_title)
+background=("#000000") # ==== BACKGROUND IS TEMP ====
+framerate=60
+font=pygame.font.Font("freesansbold.ttf", 16) # ==== FONT IS TEMP ====
+timer = pygame.time.Clock()
 
-root.mainloop()
+running=True
+while running:
+    timer.tick(framerate)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running=False
+        
+    screen.fill(background)
+    pygame.display.flip()
+
+pygame.quit()
