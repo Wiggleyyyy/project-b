89b52a1f2ad411d41187007f77ac1637b54902e6
Wiggleyyyy <141311953+Wiggleyyyy@users.noreply.github.com>
2023-10-17 16:13:41 +0200

    added colors to task

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import pygame
 pygame.init()
 
+#temp color library
+background_color="#478778" #Lincoln Green
+task_color_odd="#2AAA8A" # Jungle Green
+task_color_even="#00A36C" # Jade
+
 game_title="PROJECT B | (INSERT VERSION OF GAME LATER)" # ==== TITLE NEEDS TO BE CHANGED ====
 screen=pygame.display.set_mode([500, 500], pygame.RESIZABLE) # I made the window smaller and resizable 
 pygame.display.set_caption(game_title)
-background=("#000000") # ==== BACKGROUND IS TEMP ====
+background=(background_color) # ==== BACKGROUND IS TEMP ====
 framerate=60
 font=pygame.font.Font("freesansbold.ttf", 16) # ==== FONT IS TEMP ====
 timer = pygame.time.Clock()
 
-def draw_task(color, y_coord): # ==== COULD BE RENAMED ====
+def draw_task_1(color, y_coord): # ==== COULD BE RENAMED ====
     pygame.draw.circle(screen, color, (30, y_coord), 20, 5)
     pygame.draw.rect(screen, color, [70, y_coord - 15, 200, 30])
     pygame.draw.rect(screen, color, [75, y_coord -10, 190, 20])
@@ -22,7 +27,8 @@ while running:
             running=False
         
     screen.fill(background)
-    draw_task("#088F8F", 50)
+    draw_task_1(task_color_odd, 50)
+    draw_task_1(task_color_even, 100)
     pygame.display.flip()
 
 pygame.quit()
