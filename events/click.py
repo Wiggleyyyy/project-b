55195721fabdda4a7416bad03e725cd022d7b442
Cannon <73777697+CannonStealth@users.noreply.github.com>
2023-10-17 15:46:07 +0100

    adding a comment to click.py

--- events/click.py
+++ events/click.py
@@ -1,2 +1,4 @@
 def clicked(position):
-    print(position)
+    print(position) # prints the position of where it was clicked
+
+    # ==== NEEDS TO ADD CONDITIONALS IN ORDER TO DETECT WHERE IT WAS CLICKED ====
