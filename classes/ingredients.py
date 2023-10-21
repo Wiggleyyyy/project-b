bbcb74d758269e91df7a6104f17678fcd4760506
Cannon <73777697+CannonStealth@users.noreply.github.com>
2023-10-21 14:43:11 +0100

    Update ingredients.py

--- classes/ingredients.py
+++ classes/ingredients.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Ingredient:
     def __init__(self, price, type, icon): # to identify the ingredient 
         self.price = price # its price
@@ -7,7 +9,7 @@ class Ingredient:
 
     def buy(self, ingredients_list, money): #stil have to figure out how to do ingredients_list
         if money < self.price: return False
-        else: return money - self.price, ingredients_list
+        else: return money - self.price, np.append(ingredients_list, [self.type])
 
 
 
@@ -21,6 +23,6 @@ if button for the flour got clicked,
 attempt = flour.buy(ingredients_list)
 
 if attempt == False say that no money to buy
-if attempt returns a number then money = attempt
+else it will return the new amount of money and the updated ingredients list
 
 """
